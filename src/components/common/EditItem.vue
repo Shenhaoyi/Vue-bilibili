2ae794bff7fbcc22cd6e4085431630538dab5e03
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 21:25:49 +0800

    EditItem.vue 样式基本完成

--- src/components/common/EditItem.vue
+++ src/components/common/EditItem.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>
-    
+  <div class="wrapper">
+    <div class="title">{{left}}</div>
+    <div class="data"><slot></slot></div>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class EditItem extends Vue {
-
+    @Prop() left?: string
   }
 </script>
 
 <style lang="scss" scoped>
+  .wrapper {
+    padding:2.6667vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: white;
+    border-bottom:2px solid  #fafafa;
+    font-size: 4vw;
 
+    .title{
+      color:#505050;
+    }
+    .data{
+      color:#999;
+    }
+  }
 </style>
