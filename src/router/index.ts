f9662f5e7fa914e61709fa8257e922298682221a
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 20:35:55 +0800

    添加路由守卫。

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Register from '@/views/Register.vue';
 import Login from '@/views/Login.vue';
 import UserInfo from '@/views/UserInfo.vue';
+import {Toast} from 'vant';
 
 Vue.use(VueRouter);
 
@@ -26,12 +27,27 @@ const routes: Array<RouteConfig> = [
   {//用户信息页
     path: '/userinfo',
     name: 'UserInfo',
-    component: UserInfo
+    component: UserInfo,
+    meta: {
+      istoken: true
+    }
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  //如果是需要验证的，并且没有token或者 id的，需要重新登录
+  if (to.meta.istoken && (!localStorage.getItem('id') || !localStorage.getItem('objtoken'))) {
+      router.push('/login').then(() => {
+        Toast.fail('请重新登录！');
+      });
+    return;
+  }else{
+    next()
+  }
 });
 
 export default router;
