f1b34356de51a5b3c1b7f1477e017afe1626fbb2
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 20:03:30 +0800

    请求和响应拦截

--- http.ts
+++ http.ts
@@ -1,8 +1,34 @@
 import axios from 'axios'
+import router from '@/router';
+import {Toast} from 'vant';
 
 const http = axios.create({
   //接口
   baseURL:'http://112.74.99.5:3000/web/api'
 })
+
+
+//请求拦截
+http.interceptors.request.use(function (config) {
+  //拦截操作
+  if(localStorage.getItem('id')&&localStorage.getItem('objtoken')){
+    config.headers.Authorization = 'Bearer ' + localStorage.getItem('objtoken')
+  }
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+//响应拦截
+http.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  //重定向
+  if(error.response.status === 401 || error.response.status === 402){
+    router.push('/login').then(() => {
+      Toast.fail('请重新登录！')
+    })
+  }
+  return Promise.reject(error);
+});
 
 export default http
