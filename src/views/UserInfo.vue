6d7c2261a4257600584e30371f32e30871086888
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 15:49:04 +0800

    添加头部背景图片

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
+  <div class="user-info">
     <NavBar></NavBar>
+    <img src="../assets/bannerTop_new.png" class="background" alt="">
+    <div></div>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import NavBar from '@/components/NavBar.vue';
+  import NavBar from '@/components/common/NavBar.vue';
   @Component({
     components: {NavBar}
   })
@@ -17,5 +19,11 @@
 </script>
 
 <style lang="scss" scoped>
+  .user-info{
+    img{
+      height:131px;
+      width:100%;
+    }
+  }
 
 </style>
