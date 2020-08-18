bf497a75392fde6cadbc7c1badf2196feab41ddf
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 11:59:34 +0800

    Register.vue 将正则处理过的用户数据存在 user对象中。

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,18 +5,20 @@
                style="margin:4.167vw 0;"
                placeholder="请输入姓名"
                rule="^.{6,16}$"
-               @validInput="handle"
+               @validInput="content=> user.name = content"
     ></LoginText>
     <LoginText label="账号"
                placeholder="请输入账号"
                rule="^.{6,16}$"
+               @validInput="content=> user.accountNumbers = content"
     ></LoginText>
     <LoginText label="密码 "
                placeholder="请输入密码"
                type="password"
                rule="^.{6,16}$"
+               @validInput="content=> user.password = content"
     ></LoginText>
-    <LoginBtn btn-text="注册"></LoginBtn>
+    <LoginBtn btn-text="注册" @click.native="handle"></LoginBtn>
   </div>
 </template>
 
@@ -26,11 +28,19 @@
   import LoginTop from '@/components/common/LoginTop.vue';
   import LoginText from '@/components/common/LoginText.vue';
   import LoginBtn from '@/components/common/LoginBtn.vue';
+
   @Component({
     components: {LoginBtn, LoginText, LoginTop}
   })
   export default class Register extends Vue {
-
+    user: Record<string, any> = {
+      name: '',
+      accountNumbers: '',
+      password: ''
+    };
+    handle(){
+      console.log(this.user)
+    }
   }
 </script>
 
