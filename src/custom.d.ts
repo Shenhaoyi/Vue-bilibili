cdb69fe99b87f3a9c4e013280095c6930532e7dc
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 23:13:27 +0800

    添加 UserInfo 类

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,11 +1,11 @@
 type UserInfo = {
-  id?: number;
-  username?: string;
-  password?: string;
-  name?: string;
+  id: number;
+  username: string;
+  password: string;
+  name: string;
   user_desc: string;
-  gender?: string;
-  user_img?: string;
+  gender: string;
+  user_img: string;
 }
 
 // 类——即构造函数 ?表示可以不存在
