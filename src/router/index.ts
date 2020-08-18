64bc9de51145a715f0f5549f83ef14ceafee9550
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-18 14:26:00 +0800

    创建 UserInfo.vue，并在路由中添加。

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import Home from '../views/Home.vue';
 import Register from '@/views/Register.vue';
 import Login from '@/views/Login.vue';
+import UserInfo from '@/views/UserInfo.vue';
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes: Array<RouteConfig> = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {//用户信息页
+    path: '/userinfo',
+    name: 'UserInfo',
+    component: UserInfo
   }
 ];
 
