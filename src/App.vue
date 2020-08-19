50f427fa6b861c94224461b3274cd279f46e60f9
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-19 21:42:44 +0800

    keep-alive 结合 v-if。

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive >
+      <router-view v-if="this.$route.meta.keepalive"/>
+    </keep-alive>
+    <router-view v-if="!this.$route.meta.keepalive"/>
+
   </div>
 </template>
 
