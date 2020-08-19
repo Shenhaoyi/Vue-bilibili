dcf70c45f6e60876a6437990e30a9ad6e84bde27
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-19 09:55:14 +0800

    首页添加nav 和 tabs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-    adsad
+    <NavBar></NavBar>
+    <van-tabs v-model="active" scrollspy sticky>
+      <van-tab v-for="(item, index) in category" :key="index" :title="item.title">
+        {{ item.title }}
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-
-  @Component
+  import NavBar from '@/components/common/NavBar.vue';
+  @Component({
+    components: {NavBar}
+  })
   export default class Home extends Vue {
+    $http: any;
+
+    active=0//与导航栏的选中的下标同步
+    category=[]
+
+    created(){
+      this.selectCategory()
+    }
+
+    async selectCategory(){
+      const res= await this.$http.get('/category')
+      this.changeCategory(res.data)
+    }
+    changeCategory(data: any){
+      this.category = data
+    }
 
   }
 </script>
