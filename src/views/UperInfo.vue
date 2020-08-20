5262e9bf9c9ae65accd520582a45eb4ca53b0cd5
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-20 22:49:02 +0800

    创建up主主页。

---
+++ src/views/UperInfo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="user-info">
+    <NavBar :user-info_img="userInfo.user_img"></NavBar>
+    <img src="../assets/bannerTop_new.png" class="background" alt="">
+    <UserDetail :userInfo="userInfo"></UserDetail>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import NavBar from '@/components/common/NavBar.vue';
+  import UserDetail from '@/components/usercomponent/UserDetail.vue';
+
+  @Component({
+    components: {UserDetail, NavBar}
+  })
+  export default class UperInfo extends Vue {
+    userInfo ={}
+    $http: any;
+
+    async fetchUserInfo() {
+      const res = await this.$http.get('./user/' + localStorage.getItem('id'));
+      this.userInfo = res.data[0];
+    }
+
+    created() {
+      this.fetchUserInfo();
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .user-info {
+    img {
+      height: 100px;
+      width: 100%;
+    }
+  }
+
+</style>
