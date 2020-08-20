c9484c499dc64acf5a008027895d0d868f1366f9
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-20 16:29:06 +0800

    添加评论区的尾部

--- src/components/comment/Comment.vue
+++ src/components/comment/Comment.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <CommentItem v-for="(item, index) in comment" :key="index" :item="item">asd</CommentItem>
+  <div class="wrapper">
+    <lazy-component>
+      <CommentItem v-for="(item, index) in comment" :key="index" :item="item">asd</CommentItem>
+    </lazy-component>
+    <div class="none">
+      <span>
+        没有更多评论了~
+      </span>
+    </div>
   </div>
 </template>
 
@@ -8,6 +15,7 @@
   import Vue from 'vue';
   import {Component, Prop, Watch} from 'vue-property-decorator';
   import CommentItem from '@/components/comment/CommentItem.vue';
+
   @Component({
     components: {CommentItem}
   })
@@ -17,29 +25,35 @@
     @Prop() commentFetchFlag: any;
 
     @Watch('commentFetchFlag')
-    onFlagChanged(){
-      this.commentData()
+    onFlagChanged() {
+      this.commentData();
     }
 
-    comment=[]
+    comment = [];
 
-    created(){
-      this.commentData()
+    created() {
+      this.commentData();
     }
 
-    async commentData(){
-      const res = await this.$http.get('/comment/'+this.$route.params.id)
+    async commentData() {
+      const res = await this.$http.get('/comment/' + this.$route.params.id);
       //只取一级评论
       /* eslint-disable */
       // filter(item=> item.parent_id === null).sort((a,b)=>{ return b.comment_date - a.comment_date}).slice(0,20)
-      const len = res.data.length
-      this.comment = res.data.slice(len-20)
-      this.comment.reverse()
-      console.log(this.comment)
+      const len = res.data.length;
+      this.comment = res.data.slice(len - 20);
+      this.comment.reverse();
+      console.log(this.comment);
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  .wrapper{
+    .none {
+      padding: 22vw 0;
+      text-align: center;
+    }
+  }
 
 </style>
