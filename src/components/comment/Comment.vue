f7a36b4ec706e2a7aa95d771e8151278500cc158
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-20 16:11:48 +0800

    评论数据渲染完成

--- src/components/comment/Comment.vue
+++ src/components/comment/Comment.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    asdasdsad
+    <CommentItem v-for="(item, index) in comment" :key="index" :item="item">asd</CommentItem>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
-
-  @Component
+  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import CommentItem from '@/components/comment/CommentItem.vue';
+  @Component({
+    components: {CommentItem}
+  })
   export default class Comment extends Vue {
+    $http: any;
 
+    @Prop() commentFetchFlag: any;
+
+    @Watch('commentFetchFlag')
+    onFlagChanged(){
+      this.commentData()
+    }
+
+    comment=[]
+
+    created(){
+      this.commentData()
+    }
+
+    async commentData(){
+      const res = await this.$http.get('/comment/'+this.$route.params.id)
+      //只取一级评论
+      /* eslint-disable */
+      // filter(item=> item.parent_id === null).sort((a,b)=>{ return b.comment_date - a.comment_date}).slice(0,20)
+      const len = res.data.length
+      this.comment = res.data.slice(len-20)
+      this.comment.reverse()
+      console.log(this.comment)
+    }
   }
 </script>
 
