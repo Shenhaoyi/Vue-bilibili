cef5b92c66e2d1ceee159bec3a013e27e57533e0
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-20 12:57:10 +0800

    创建评论组件

---
+++ src/components/article/Comment.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    asdas
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Comment extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
