6db9a5f890830b37bc0446732d28cb4de9112424
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-08-22 19:25:59 +0800

    去掉代理

--- vue.config.js
+++ vue.config.js
@@ -7,25 +7,25 @@ module.exports = {
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: true,
 
-  devServer: {
-    host: 'localhost',
-    port: 8000, // 端口号
-    // https: false, // https:{type:Boolean}
-    open: true, //配置自动启动浏览器  http://172.11.11.22:8888/rest/XX/
-    // hotOnly: true, // 热更新
-    // proxy: 'http://localhost:8000'   // 配置跨域处理,只有一个代理
-    proxy: { //配置自动启动浏览器
-      "/web": {
-        target: "http://112.74.99.5:3000",
-        changeOrigin: true,
-        // ws: true,//websocket支持
-        // secure: false,
-        // pathRewrite: {
-        //   '/web/api': 'http://112.74.99.5:3000/web/api'
-        // }
-      }
-    }
-  },
+  // devServer: {
+  //   host: 'localhost',
+  //   port: 8000, // 端口号
+  //   // https: false, // https:{type:Boolean}
+  //   open: true, //配置自动启动浏览器  http://172.11.11.22:8888/rest/XX/
+  //   // hotOnly: true, // 热更新
+  //   // proxy: 'http://localhost:8000'   // 配置跨域处理,只有一个代理
+  //   proxy: { //配置自动启动浏览器
+  //     "/web": {
+  //       target: "http://112.74.99.5:3000",
+  //       changeOrigin: true,
+  //       // ws: true,//websocket支持
+  //       // secure: false,
+  //       // pathRewrite: {
+  //       //   '/web/api': 'http://112.74.99.5:3000/web/api'
+  //       // }
+  //     }
+  //   }
+  // },
 
 
 }
